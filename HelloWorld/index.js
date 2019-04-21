c462e5ef964201b2afbd57e344bb786d4777c790
MAJU STUDENT <majustudent@jinnah.edu>
2019-04-21 16:40:05 +0500

    First commit

---
+++ HelloWorld/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import Basic_code from './Basic_code';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Basic_code);
